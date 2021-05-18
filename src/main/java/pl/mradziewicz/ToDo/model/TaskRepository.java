9870ba85bd580c4d9025c25a242bd9fd1cb6914b
Radzi <radziewicz94@gmail.com>
2021-05-18 22:14:34 +0200

    zablokowanie możliwości usuwania encji

--- src/main/java/pl/mradziewicz/ToDo/model/TaskRepository.java
+++ src/main/java/pl/mradziewicz/ToDo/model/TaskRepository.java
@@ -1,8 +1,17 @@
 package pl.mradziewicz.ToDo.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@RepositoryRestController
+@RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
 }
